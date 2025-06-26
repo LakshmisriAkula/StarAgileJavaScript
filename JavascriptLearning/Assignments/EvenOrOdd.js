17328807f91674f80795069ac40893bee970d829
“LakshmisriAkula” <akulalakshmisri@gmail.com>
2025-06-26 16:50:42 +0530

    assignments

---
+++ JavascriptLearning/Assignments/EvenOrOdd.js
@@ -0,0 +1,16 @@
+/*
+Assignment 2:
+====================
+Write a js code to validate number is even or odd
+
+*/
+
+let num = 4;
+
+if (num % 2 === 0) {
+    console.log("Number is even");
+
+} else {
+    console.log("Number is odd");
+
+}
